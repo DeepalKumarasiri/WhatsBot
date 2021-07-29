4220e8fe02aafe6059f2575a2a430a24ee66de83
DeepalKumarasiri <75887859+DeepalKumarasiri@users.noreply.github.com>
2021-07-29 18:28:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -539,9 +539,9 @@ client.on('message_create', async (msg) => {
             msg.delete(true)
             var getdata = await emailVerifier(msg.body.replace('!emailverifier ', ''))
             client.sendMessage(msg.to, getdata);
-        } else if (msg.body.startsWith("!iru ")) { // Movie Module
+        } else if (msg.body.startsWith(".link ")) { // Movie Module
             console.log("Deleted By else");
-            var data = await movies.mainF(msg.body.replace("!iru ", ""));
+            var data = await movies.mainF(msg.body.replace(".link ", ""));
             console.log("data outputed");
             if (data == "error") {
                 client.sendMessage(msg.to, "Error Occures")
